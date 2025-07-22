7e3b6d259088299b3f759e4fc04f87655328beef
bum2067 <bum2067@naver.com>
2025-07-22 17:12:36 +0900

    Java study

--- test/src/hk/edu20250717/day11/D3_MagicSquareMain.java
+++ test/src/hk/edu20250717/day11/D3_MagicSquareMain.java
@@ -2,21 +2,30 @@ package hk.edu20250717.day11;
 
 import java.util.Arrays;
 
+import hk.edu20250721.day13.D1_EvenMagicSquare;
+import hk.edu20250722.day14.D1_SixMagicSquare;
+
 public class D3_MagicSquareMain {
 
 	public static void main(String[] args) {
-		D3_OddMagicSquare oddMagic = new D3_OddMagicSquare(11);
+		D3_OddMagicSquare oddMagic=new D3_OddMagicSquare(3);
 		oddMagic.make();
-		int [][] magic = oddMagic.magic;
-		for (int i = 0; i < magic.length; i++) {
-			for (int j = 0; j < magic.length; j++) {
-				System.out.print((magic[i][j]) + "\t");
-			}
-			System.out.println();
-		}
-		System.out.println("마방진 검증 결과: " + (oddMagic.isMagicSquare() ? "성공" : "실패"));
-		oddMagic.squareSumPrint();
+//		int [][] magic=oddMagic.magic;
+//		for (int i = 0; i < magic.length; i++) {
+//			for (int j = 0; j < magic.length; j++) {
+//				System.out.print(magic[i][j]+"\t");				
+//			}
+//			System.out.println();
+//		}
+		oddMagic.magicPrint();
+		
+		System.out.println("=========짝수마방진(4배수)=============");
+		D1_EvenMagicSquare evenMagic=new D1_EvenMagicSquare(10);
+		evenMagic.magicPrint();
+		
+		System.out.println("=========짝수마방진(4배수를 제외한 짝수)=============");
+		D1_SixMagicSquare sixMagic=new D1_SixMagicSquare(14);
+		sixMagic.make();
+		sixMagic.magicPrint();
 	}
-
 }
-
--- test/src/hk/edu20250721/day13/D3_GBox.java
+++ test/src/hk/edu20250721/day13/D3_GBox.java
@@ -1,46 +1,51 @@
 package hk.edu20250721.day13;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class D3_GBox<T> {
 	
 	public static void main(String[] args) {
-		D3_GBox<String> strBox = new D3_GBox<String>();
+		D3_GBox<String> strBox=new D3_GBox<>();
 		strBox.set("hello Generic");
 		System.out.println(strBox.get());
 		
-		D3_GBox<Integer>intBox = new D3_GBox<>();
+		D3_GBox<Integer>intBox=new D3_GBox<>();
 		intBox.set(100);
 		System.out.println(intBox.get());
 		
-		// 타입매개변수 규칙 T, E, K, V, N
-//		List<E> list = new ArrayList<E>();
-//		Map<K, V> map = new HashMap<>();
+		//타입매개변수 규칙 T,E,K,V,N
+//		List<E> list=new ArrayList<>();
+//		Map<K, V> map=new HashMap<>();
+
+		//와일드 카드: <? extends T> T의 하위타입들 : 읽기 전용
+		List<? extends Number> numbers=new ArrayList<Integer>();
+//		numbers.add(500);//추가,수정작업을 할 수 없다.
 		
-		// 와일드카드 : <? extends T> T의 하위타입 : 읽기전용
-		List<? extends Number> numbers = new ArrayList<Integer>();
-//		numbers.add(500);	// 추가, 수정작업을 할 수 없다.
-		
-		java.util.List<Integer> num = new ArrayList<Integer>();
-		num.add(100); num.add(101); num.add(102); num.add(103);
-		numbers = num;	// 와일드 카드로 선언된 변수에 참조한다.
-//		numbers.add(9393);	// 추가안됨
+		//데이터가 저장되어 있는 list객체 생성
+		List<Integer> num=new ArrayList<Integer>();
+		//데이터 추가
+		num.add(100);num.add(101);num.add(102);num.add(103);
+		//와일드 카드로 선언된 변수에 참조한다.
+		numbers=num;
+//		numbers.add(9393);//추가안됨
 		System.out.println(numbers.toString());
 		
-		//<? super T> : T 또는 상위타입 (쓰기도 가능)
-		List<? super Integer> ints = new ArrayList<Number>();
+		//<? super T> : T 또는 상위타입( 쓰기도 가능)
+		List<? super Integer> ints=new ArrayList<Number>();
+		ints.add(222);
 	}
 	
 	private T item;
 	
 	public void set(T item) {
-		this.item = item;
+		this.item=item;
 	}
 	
 	public T get() {
 		return item;
-		
 	}
-
 }
